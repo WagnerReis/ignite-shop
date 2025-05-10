fa637d4d951f0ec23f02081e03ed07a51315f947
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-10 16:16:38 -0300

    feat(product): implement product detail page layout and styling

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,13 +1,28 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "./styles"
+
 interface ProductProps {
-  params: { id: string}
+  params: { id: string }
 }
 
-export default async function Product({params}: ProductProps) {
+export default async function Product({ params }: ProductProps) {
   const { id } = await params
 
   return (
-    <>
-      <h1>Product: {id}</h1>
-    </>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, deserunt qui saepe, eum consequatur illo totam maxime rem modi, ad doloribus at blanditiis. Doloremque odio corrupti, recusandae explicabo cum numquam?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
