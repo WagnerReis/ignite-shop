d38718ce5f8d19c02e6d6fe8a9885d223733e7a0
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-12 14:53:55 -0300

    feat(checkout): add session ID to success URL and implement success page

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: 'Price not found.' }, { status: 400 });
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,17 +1,43 @@
 import Link from "next/link";
 import { ImageContainer, SuccessContainer } from "./styles";
+import { redirect, useSearchParams } from "next/navigation";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
 
-export default function Success() {
+type SuccessPageProps = {
+  searchParams: { session_id?: string };
+};
+
+
+export default async function Success({
+  searchParams,
+}: SuccessPageProps) {
+  const sessionId = (await searchParams).session_id;
+
+  console.log('sessionId: ', sessionId);
+
+  if (!sessionId) {
+    redirect('/');
+  }
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product']
+  })
+
+  const customerName = session.customer_details?.name;
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
   return (
     <SuccessContainer >
       <h1>Compra efetuada</h1>
 
       <ImageContainer>
-
+        <Image src={product.images[0]} width={120} height={110} alt="" />
       </ImageContainer>
 
       <p>
-        Uhuul <strong>Gabriel</strong>, sua compra de <strong>Camiseta X</strong> já está a caminho da sua casa.
+        Uhuul <strong>{customerName}</strong>, sua compra de <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">
