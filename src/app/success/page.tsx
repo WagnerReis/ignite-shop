ff104478104905657681496bfbc2d9abd69ed08b
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-12 13:46:32 -0300

    feat(success): implement success page layout and styling

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "./styles";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer >
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Gabriel</strong>, sua compra de <strong>Camiseta X</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
 }
